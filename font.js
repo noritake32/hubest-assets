6e56e3472fef257089e5b73c585f25574b7f1b87
noritake32 <n-takeda@hubest.jp>
2025-06-11 21:20:15 +0900

    Add files via upload

---
+++ font.js
@@ -0,0 +1,6 @@
+(function () {
+  const link = document.createElement('link');
+  link.href = 'https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+})();
